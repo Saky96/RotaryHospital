d33ee85a1c1e309430f707da16828149f5ad5136
megha2496 <jain.megha2496@gmail.com>
2017-09-13 21:50:04 +0530

    added elements to appointment details fragment

--- app/src/main/java/com/example/jain/rotaryhospital/BookAppointmentFragment.java
+++ app/src/main/java/com/example/jain/rotaryhospital/BookAppointmentFragment.java
@@ -1,9 +1,11 @@
 package com.example.jain.rotaryhospital;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.icu.util.Calendar;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
@@ -19,6 +23,10 @@ public class BookAppointmentFragment extends Fragment {
 
     Spinner doclist;
     String[] items;
+    EditText etDate;
+    int mYear;
+    int mMonth;
+    int mDay;
 
     public BookAppointmentFragment() {
         // Required empty public constructor
@@ -26,47 +34,41 @@ public class BookAppointmentFragment extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-       /* doclist = (Spinner) findViewById(R.id.SpinnerBookAppointmentFragmentDoctorList);
-        items = new String[]{"Dr. Kailash Gupta", "Dr. Satinder Pal", "Dr. Kanak Anand", "Dr. Manika Singh", "Dr. Kalindi Khera"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(BookAppointmentFragment.this, android.R.layout.simple_spinner_dropdown_item, items);
-        doclist.setAdapter(adapter); */
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.book_appointment_fragment, container, false);
 
-        //?? problem here in getting context
-       /* LayoutInflater inflater1= (LayoutInflater) getParentFragment().getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater1.inflate(R.layout.spinner_view, container, false);
-        items = new String[]{"Dr. Kailash Gupta", "Dr. Satinder Pal", "Dr. Kanak Anand", "Dr. Manika Singh", "Dr. Kalindi Khera"};
-        Spinner doclistspinner = (Spinner) v.findViewById(R.id.SpinnerBookAppointmentFragmentDoctorList);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_spinner_item, items);
-        doclistspinner.setAdapter(adapter);
-        return v;*/
     }
 
-    /*public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+/*
+    yourEditText.setOnClickListener(new OnClickListener() {
 
-        // Create a new instance of TimePickerDialog and return it
-        return new TimePickerDialog(getActivity(), this, hour, minute,
-                DateFormat.is24HourFormat(getActivity()));
-    }
+        @Override
+        public void onClick(View v) {
+            //To show current date in the datepicker
+            Calendar mcurrentDate=Calendar.getInstance();
+            mYear=mcurrentDate.get(Calendar.YEAR);
+            mMonth=mcurrentDate.get(Calendar.MONTH);
+            mDay=mcurrentDate.get(Calendar.DAY_OF_MONTH);
 
+            DatePickerDialog mDatePicker=new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                    *//*      Your code   to get date and time    *//*
+                }
+            },mYear, mMonth, mDay);
+            mDatePicker.setTitle("Select date");
+            mDatePicker.show();  }
+    });*/
 
     @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        doclist = (Spinner) getActivity().findViewById(R.id.SpinnerBookAppointmentFragmentDoctorList);
+        items = new String[]{"Dr. Kailash Gupta (Skin specialist)", "Dr. Satinder Pal (heart specialist)", "Dr. Kanak Anand (ENT specialist)", "Dr. Manika Singh (gynecologist)", "Dr. Kalindi Khera (pediatrician)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+        doclist.setAdapter(adapter);
 
-    }*/
+    }
 }
 
