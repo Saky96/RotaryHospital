42b0db11a2ae60db43ff4711cb2fbd4825c76167
megha2496 <jain.megha2496@gmail.com>
2017-09-14 07:22:19 +0530

    improvements in the appointment details fragment

--- app/src/main/java/com/example/jain/rotaryhospital/BookAppointmentFragment.java
+++ app/src/main/java/com/example/jain/rotaryhospital/BookAppointmentFragment.java
@@ -19,14 +19,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BookAppointmentFragment extends Fragment {
 
     Spinner doclist;
-    String[] items;
-    EditText etDate;
-    int mYear;
-    int mMonth;
-    int mDay;
+    DatePicker pickerDate;
+    TimePicker pickerTime;
 
     public BookAppointmentFragment() {
         // Required empty public constructor
@@ -41,33 +41,34 @@ public class BookAppointmentFragment extends Fragment {
 
     }
 
-/*
-    yourEditText.setOnClickListener(new OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            //To show current date in the datepicker
-            Calendar mcurrentDate=Calendar.getInstance();
-            mYear=mcurrentDate.get(Calendar.YEAR);
-            mMonth=mcurrentDate.get(Calendar.MONTH);
-            mDay=mcurrentDate.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog mDatePicker=new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-                public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
-                    *//*      Your code   to get date and time    *//*
-                }
-            },mYear, mMonth, mDay);
-            mDatePicker.setTitle("Select date");
-            mDatePicker.show();  }
-    });*/
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         doclist = (Spinner) getActivity().findViewById(R.id.SpinnerBookAppointmentFragmentDoctorList);
-        items = new String[]{"Dr. Kailash Gupta (Skin specialist)", "Dr. Satinder Pal (heart specialist)", "Dr. Kanak Anand (ENT specialist)", "Dr. Manika Singh (gynecologist)", "Dr. Kalindi Khera (pediatrician)"};
-        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Doctor_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         doclist.setAdapter(adapter);
+
+
+        pickerDate = (DatePicker) getActivity().findViewById(R.id.datePickerBookAppointmentFragmentDate);
+        pickerTime = (TimePicker) getActivity().findViewById(R.id.timePickerBookAppointmentFragmentTime);
+        java.util.Calendar calender = java.util.Calendar.getInstance();
+        calender.clear();
+        calender.set(java.util.Calendar.MONTH, pickerDate.getMonth());
+        calender.set(java.util.Calendar.DAY_OF_MONTH, pickerDate.getDayOfMonth());
+        calender.set(java.util.Calendar.YEAR, pickerDate.getYear());
+        calender.set(java.util.Calendar.HOUR, pickerTime.getCurrentHour());
+        calender.set(java.util.Calendar.MINUTE, pickerTime.getCurrentMinute());
+        calender.set(java.util.Calendar.SECOND, 00);
+
+        SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.hour_minutes));
+        String timeString = formatter.format(new Date(calender.getTimeInMillis()));
+        SimpleDateFormat dateformatter = new SimpleDateFormat(getString(R.string.dateformate));
+        String dateString = dateformatter.format(new Date(calender.getTimeInMillis()));
+
+        /* The variables timeString and dateString are to be used in the database...refer memo app Activity_create_note*/
 
     }
 }
