1c9ea490faa3d6a22939f8bf250b0f19fcf98201
megha2496 <jain.megha2496@gmail.com>
2017-09-15 22:16:06 +0530

    added alert dialog box to info button (text is only sample, can be changed) added age edittext to registration form

--- app/src/main/java/com/example/jain/rotaryhospital/HomeActivity.java
+++ app/src/main/java/com/example/jain/rotaryhospital/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.jain.rotaryhospital;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,19 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String msg = getString(R.string.info_msg);
+        builder.setMessage(msg).setCancelable(
+                false).setPositiveButton(getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(HomeActivity.this, HomeActivity.class));
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();*/
+
+
     }
 
     public void DoctorsAvailable(View view) {
@@ -25,4 +40,22 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void InfoAlert(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String msg = getString(R.string.info_alert_title)+ getString(R.string.info_alert_msg);
+        builder.setMessage(msg).setCancelable(
+                false).setPositiveButton(getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent= new Intent(HomeActivity.this, HomeActivity.class);
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
 }
